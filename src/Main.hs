a8c982a1060d57dca8aed3d5765cedfbdfb814b9
lopezfjose <jflopezfernandez@gmail.com>
2018-11-22 18:10:21 -0500

    Refactored

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,19 @@
 
 import System.IO
 
-
-main :: IO ()
-main = do
-    putStrLn "<Omega Compiler Whitespace Length Analysis>"
+loop :: IO ()
+loop = do
     putStr "Enter string: "
     hFlush stdout
     line <- getLine
     if (length line) == 0 then
-            return ()
+        return ()
     else do
         putStrLn $ "Length: " ++ (show (length line))
-        main
+        loop
+
+
+main :: IO ()
+main = do
+    putStrLn "<Omega Compiler Whitespace Length Analysis>"
+    loop
