c55f1598a1822b7ae1957ef48ea41b817d7d9941
lopezfjose <jflopezfernandez@gmail.com>
2018-11-22 19:04:11 -0500

    Numbers are now being properly recognized

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,38 @@
 
+import Data.Char
 import System.IO
+
+showLength :: String -> String
+showLength str = show (length str)
+
+data Token = TokenOp Op
+           | TokenNum Int
+           | TokenEnd
+    deriving (Show, Eq)
+
+data Op = Add | Subtract deriving (Show, Eq)
+
+op :: String -> Op
+op str
+    | str == "\t"   = Add
+    | str == "\t\t" = Subtract
+    | otherwise     = error "<Other Op>"
+
+accumulateSpaces :: String -> String -> [Token]
+accumulateSpaces acc [] = [(number acc)]
+accumulateSpaces acc (x:xs)
+    | [x] == " " = accumulateSpaces (acc ++ [x]) xs
+    | otherwise = number acc : tokenize xs
+
+number :: String -> Token
+number str = TokenNum (length str)
+
+tokenize :: String -> [Token]
+tokenize "" = []
+tokenize (x:xs)
+    | [x] == "\t" = TokenOp (op [x]) : tokenize xs
+    | [x] == " " = accumulateSpaces [x] xs
+    | otherwise = error "Other"
 
 loop :: IO ()
 loop = do
@@ -8,12 +41,14 @@ loop = do
     line <- getLine
     if (length line) == 0 then
         return ()
-    else do
-        putStrLn $ "Length: " ++ (show (length line))
-        loop
+    else
+        let
+            token = tokenize (line :: String)
+        in do
+            (print . show) token
+            main
 
 
 main :: IO ()
 main = do
-    putStrLn "<Omega Compiler Whitespace Length Analysis>"
     loop
